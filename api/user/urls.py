683f74b4806472df06ef151e1e708e6f98997b93
Aldrich Escudero <aldrich.escudero@gmail.com>
2022-10-09 17:15:54 +0800

    Updated User API endpoints

--- api/user/urls.py
+++ api/user/urls.py
@@ -8,4 +8,8 @@ urlpatterns = [
     
     path('create-user/', views.add_users, name='create-user'),
     path("current-user/", views.current_user, name='current-user'),   
+    path("delete-user/", views.delete_user, name='delete-user'),   
+    path("restore-user/", views.restore_user, name='restore-user'),   
+    path("list-users/", views.list_users, name='list-users'),   
+    path("archived-users/", views.list_archived_users, name='archived-users'),   
 ]
--- api/user/views.py
+++ api/user/views.py
@@ -2,8 +2,10 @@ from rest_framework import views, response, exceptions, permissions
 from rest_framework.decorators import api_view, authentication_classes, permission_classes
 from rest_framework.response import Response
 
+from . models import User
 from . import serializer as user_serializer
 from . import services, authentication
+from datetime import datetime
 
 
 @api_view(['POST'])
@@ -51,6 +53,112 @@ def current_user(request):
     serializer = user_serializer.UserSerializer(user)
 
     return response.Response(serializer.data)
+
+
+@api_view(['GET'])
+@authentication_classes([authentication.CustomUserAuthentication])
+@permission_classes([permissions.IsAuthenticated])
+def list_users(request):
+    
+    # checking for the parameters from the URL
+    users = User.objects.filter(is_active=True)
+    serializer = user_serializer.UserSerializer(users, many=True)        
+  
+    # if there is something in items else raise error
+    if users:
+        
+        return Response(serializer.data)
+    else:
+
+        raise exceptions.NotFound({"message" : "No active users."})
+    
+    
+@api_view(['GET'])
+@authentication_classes([authentication.CustomUserAuthentication])
+@permission_classes([permissions.IsAuthenticated])
+def list_archived_users(request):
+    
+    # checking for the parameters from the URL
+    users = User.objects.filter(is_active=False)
+    serializer = user_serializer.UserSerializer(users, many=True)        
+  
+    # if there is something in items else raise error
+    if users:
+        
+        return Response(serializer.data)
+    else:
+
+        raise exceptions.NotFound({"message" : "No archived users."})
+  
+
+
+@api_view(['POST'])
+@authentication_classes([authentication.CustomUserAuthentication])
+@permission_classes([permissions.IsAuthenticated])
+def delete_user(request):
+    
+    data = request.data
+        
+    user_email=data["email"]
+   
+    to_delete = User.objects.filter(
+        is_active=True,
+        email=user_email
+    ).update(
+        is_active=False,
+        updated_at = datetime.now().strftime('%Y-%m-%dT%H:%M:%SZ'),
+        deleted_at = datetime.now().strftime('%Y-%m-%dT%H:%M:%SZ')
+    )
+    
+    if to_delete:     
+        
+        response = { "message" : "Successfully deleted!"}
+        
+        return Response(response, status=202)
+    
+    else:
+        raise exceptions.ParseError({"message" : "Invalid argument."})
+    
+
+@api_view(['POST'])
+@authentication_classes([authentication.CustomUserAuthentication])
+@permission_classes([permissions.IsAuthenticated])
+def restore_user(request):
+    
+    data = request.data
+        
+    user_email=data["email"]
+   
+    to_delete = User.objects.filter(
+        is_active=False,
+        email=user_email
+    ).update(
+        is_active=True,
+        updated_at = datetime.now().strftime('%Y-%m-%dT%H:%M:%SZ'),
+        deleted_at = None
+    )
+    
+    if to_delete:     
+        
+        response = { "message" : "Successfully restored!"}
+        
+        return Response(response, status=202)
+    
+    else:
+        raise exceptions.ParseError({"message" : "Invalid argument."})
+    
+
+@api_view(['POST'])
+@authentication_classes([authentication.CustomUserAuthentication])
+@permission_classes([permissions.IsAuthenticated])
+def logout(request):
+    
+    
+    resp = response.Response()
+    resp.delete_cookie("jwt")
+    resp.data = {"message": "Successfully logged out."}
+
+    return resp
 
 
 @api_view(['POST'])
