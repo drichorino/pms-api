36a51b85707ffd3b58f3adf2255b16fb37e4e1b9
Aldrich Escudero <aldrich.escudero@gmail.com>
2022-10-11 22:19:27 +0800

    Updated User urls

--- api/user/urls.py
+++ api/user/urls.py
@@ -6,7 +6,7 @@ urlpatterns = [
     path("login/", views.login, name='login'),
     path("logout/", views.logout, name='logout'),
     
-    path('create-user/', views.add_users, name='create-user'),
+    path("create-user/", views.add_users, name='create-user'),
     path("current-user/", views.current_user, name='current-user'),   
     path("delete-user/", views.delete_user, name='delete-user'),   
     path("restore-user/", views.restore_user, name='restore-user'),   
