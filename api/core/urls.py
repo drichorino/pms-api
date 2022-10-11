48560067db5b8a3cc1cb8c0c305f667992e70df6
Aldrich Escudero <aldrich.escudero@gmail.com>
2022-10-11 22:20:34 +0800

    Added payroll app urls to core urls

--- api/core/urls.py
+++ api/core/urls.py
@@ -19,4 +19,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('pms-api/', include('user.urls')),
+    path('pms-api/', include('payroll.urls')),
 ]
