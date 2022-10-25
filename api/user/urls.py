b4dc07e32022094b20dbeb59fe40f5216fa54648
Aldrich Escudero <aldrich.escudero@gmail.com>
2022-10-25 23:11:31 +0800

    Updated user app urls

--- api/user/urls.py
+++ api/user/urls.py
@@ -6,10 +6,11 @@ urlpatterns = [
     path("login/", views.login, name='login'),
     path("logout/", views.logout, name='logout'),
     
-    path("create-user/", views.add_users, name='create-user'),
-    path("current-user/", views.current_user, name='current-user'),   
-    path("delete-user/", views.delete_user, name='delete-user'),   
-    path("restore-user/", views.restore_user, name='restore-user'),   
-    path("list-users/", views.list_users, name='list-users'),   
-    path("archived-users/", views.list_archived_users, name='archived-users'),   
+    path("users/create/", views.add_users, name='create-user'),
+    path("users/current/", views.current_user, name='current-user'),
+    path("users/update/", views.update_user, name='update-user'),   
+    path("users/deactivate/", views.deactivate_user, name='deactivate-user'),   
+    path("users/restore/", views.restore_user, name='restore-user'),   
+    path("users/list/", views.list_users, name='list-users'),   
+    path("users/archive/", views.list_archived_users, name='archived-users'),   
 ]
