6992d3d51627e4466478fa88259bb89131401c01
Aldrich Escudero <aldrich.escudero@gmail.com>
2022-10-25 23:10:33 +0800

    Created Projects API Endpoints

--- api/payroll/urls.py
+++ api/payroll/urls.py
@@ -4,13 +4,18 @@ from .views import sites, projects, employees, payslip, daily_time_records
 urlpatterns = [
     
     ### SITES ###
-    path("add-site/", sites.add_site, name='add-site'),   
+    path("sites/add/", sites.add_site, name='add-site'),   
     
     ### PROJECTS ###
-    path("add-project/", projects.add_project, name='add-project'),
+    path("projects/add/", projects.add_project, name='add-project'),
+    path("projects/list/", projects.list_projects, name='list-projects'),
+    path("projects/archive/", projects.list_archived_projects, name='list-archived-projects'),
+    
+    path("projects/deactivate/", projects.deactivate_project, name='deactivate-project'),
+    path("projects/restore/", projects.restore_project, name='restore-project'),
     
     ### EMPLOYEES ###
-    path("add-employee/", employees.add_employee, name='add-employee'),
+    path("employees/add/", employees.add_employee, name='add-employee'),
     
     
     
--- api/payroll/views/projects.py
+++ api/payroll/views/projects.py
@@ -7,6 +7,8 @@ from user import authentication
 from .. models import Project
 from .. serializers import ProjectSerializer
 
+from datetime import datetime
+
 
 @api_view(['POST'])
 def add_project(request):
@@ -24,5 +26,105 @@ def add_project(request):
         raise exceptions.ParseError({ "message" : f"Unable to add project, {project_name}."})
     
     return Response({ "message" : f"Project {project_name} is added successfully!"})
+
+
+@api_view(['GET'])
+@authentication_classes([authentication.CustomUserAuthentication])
+@permission_classes([permissions.IsAuthenticated])
+def list_projects(request):
+    
+    # checking for the parameters from the URL
+    projects = Project.objects.filter(is_active=True)
+    serializer = ProjectSerializer(projects, many=True)        
+  
+    # if there is something in items else raise error
+    if projects:
+        
+        return Response(serializer.data)
+    else:
+
+        raise exceptions.NotFound({"message" : "No active projects."})
+    
+    
+@api_view(['GET'])
+@authentication_classes([authentication.CustomUserAuthentication])
+@permission_classes([permissions.IsAuthenticated])
+def list_archived_projects(request):
+    
+    # checking for the parameters from the URL
+    projects = Project.objects.filter(is_active=False)
+    serializer = ProjectSerializer(projects, many=True)
+  
+    # if there is something in items else raise error
+    if projects:
+        
+        return Response(serializer.data)
+    else:
+
+        raise exceptions.NotFound({"message" : "No archived projects."})
+    
+    
+@api_view(['POST'])
+@authentication_classes([authentication.CustomUserAuthentication])
+@permission_classes([permissions.IsAuthenticated])
+def deactivate_project(request):
+    
+    data = request.data
+        
+    project_id = data["id"]
+    
+    project = Project.objects.get(id=project_id)
+    serializer = ProjectSerializer(project)
+    project_name = serializer.data["name"]   
+     
+    to_deactivate = Project.objects.filter(
+        is_active=True,
+        id=project_id
+    ).update(
+        is_active=False,
+        updated_at = datetime.now().strftime('%Y-%m-%dT%H:%M:%SZ'),
+        deleted_at = datetime.now().strftime('%Y-%m-%dT%H:%M:%SZ')
+    )
+    
+    if to_deactivate:     
+        
+        response = { "message" : f"Project {project_name} has been successfully archived."}
+        
+        return Response(response, status=202)
+    
+    else:
+        raise exceptions.ParseError({"message" : f"Project {project_name} cannot be found or is already archived."})
+    
+    
+@api_view(['POST'])
+@authentication_classes([authentication.CustomUserAuthentication])
+@permission_classes([permissions.IsAuthenticated])
+def restore_project(request):
+    
+    data = request.data
+        
+    project_id = data["id"]
+    
+    project = Project.objects.get(id=project_id)
+    serializer = ProjectSerializer(project)
+    project_name = serializer.data["name"]   
+     
+    to_activate = Project.objects.filter(
+        is_active=False,
+        id=project_id
+    ).update(
+        is_active=True,
+        updated_at = datetime.now().strftime('%Y-%m-%dT%H:%M:%SZ'),
+        deleted_at = None
+    )
+    
+    if to_activate:     
+        
+        response = { "message" : f"Project {project_name} has been successfully re-activated."}
+        
+        return Response(response, status=202)
+    
+    else:
+        raise exceptions.ParseError({"message" : f"Project {project_name} cannot be found or is already activated."})
 
 
