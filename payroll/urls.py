e42c06be3dc75f86534567a1bc6663f260258a24
Aldrich Escudero <aldrich.escudero@gmail.com>
2022-10-28 19:54:03 +0800

    Updated payroll app urls

--- payroll/urls.py
+++ payroll/urls.py
@@ -4,13 +4,18 @@ from .views import sites, projects, employees, payslip, daily_time_records
 urlpatterns = [
     
     ### SITES ###
-    path("sites/add/", sites.add_site, name='add-site'),   
+    path("sites/add/", sites.add_site, name='add-site'),
+    path("sites/list/", sites.list_sites, name='list-sites'), 
+    path("sites/archive/", sites.list_archived_sites, name='list-archived-sites'), 
+    path("sites/edit/", sites.edit_site, name='edit-site'),    
+    path("sites/deactivate/", sites.deactivate_site, name='deactivate-sites'), 
+    path("sites/restore/", sites.restore_site, name='restore-site'), 
     
     ### PROJECTS ###
     path("projects/add/", projects.add_project, name='add-project'),
     path("projects/list/", projects.list_projects, name='list-projects'),
     path("projects/archive/", projects.list_archived_projects, name='list-archived-projects'),
-    
+    path("projects/edit/", projects.edit_project, name='edit-project'),
     path("projects/deactivate/", projects.deactivate_project, name='deactivate-project'),
     path("projects/restore/", projects.restore_project, name='restore-project'),
     
