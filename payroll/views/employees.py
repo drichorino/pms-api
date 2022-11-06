dca792b9d1e587be80157ca1f8df6bbf88501652
Aldrich Escudero <aldrich.escudero@gmail.com>
2022-11-07 00:12:28 +0800

    Update employees.py

--- payroll/views/employees.py
+++ payroll/views/employees.py
@@ -7,27 +7,13 @@ from user import authentication
 from .. models import Employee
 from .. serializers import EmployeeSerializer
 
+from helper.response_helper import ResponseHelper
+
+from django.utils import timezone
+
 
 @api_view(['POST'])
-def add_employee(request):
-    serializer = EmployeeSerializer(data=request.data)
-    serializer.is_valid(raise_exception=True)
-
-    data = serializer.validated_data
+def add_site(request):
     
-    empolyee_name = data["name"]
-    site = Employee(
-            name=empolyee_name,
-            position=data["position"],
-            site=data["site"],
-            basic_rate=data["basic_rate"]
-            )
-    
-    try:
-        site.save()        
-    except:
-        raise exceptions.ParseError({ "message" : f"Unable to add employee, {empolyee_name}."})
-    
-    return Response({ "message" : f"Employee {empolyee_name} is added successfully!"})
-
+    data=request.data
 
